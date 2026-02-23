90aa1cc8ee0846754ec827d120e53ff804ef222e
Jeffrey Thies <KINGMET@Jeffreys-iMac.lan>
2026-02-23 14:42:17 -0500

    Return stems from GPU

--- rebuilder.py
+++ rebuilder.py
@@ -15,6 +15,7 @@ image = (
 @modal.fastapi_endpoint(method="POST")
 def separate_audio(file_bytes: bytes, filename: str):
     import os
+    import base64
 
     os.makedirs("/tmp/input", exist_ok=True)
     os.makedirs("/tmp/output", exist_ok=True)
@@ -22,8 +23,17 @@ def separate_audio(file_bytes: bytes, filename: str):
     input_path = f"/tmp/input/{filename}"
 
     with open(input_path, "wb") as f:
-        f.write(file_bytes)
+        f.write(bytes(file_bytes))
 
     os.system(f"demucs -o /tmp/output {input_path}")
 
-    return {"status": "Separated!"}
+    stem_folder = f"/tmp/output/htdemucs/{filename.split('.')[0]}"
+
+    stems = {}
+
+    for stem in ["vocals", "drums", "bass", "other"]:
+        path = f"{stem_folder}/{stem}.wav"
+        with open(path, "rb") as s:
+            stems[stem] = base64.b64encode(s.read()).decode("utf-8")
+
+    return stems
